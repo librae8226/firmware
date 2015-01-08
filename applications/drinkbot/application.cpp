91d5aa7348707a1eb7c8bdad819358b20079e269
Librae <librae8226@gmail.com>
2015-01-08 22:41:39 +0800

    remove tinker funcs

--- applications/drinkbot/application.cpp
+++ applications/drinkbot/application.cpp
@@ -47,6 +47,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "application.h"
+#include "OneWire.h"
+#include "spark-dallas-temperature.h"
 
 /* Definitions ---------------------------------------------------------------*/
 SYSTEM_MODE(AUTOMATIC);
@@ -74,10 +76,6 @@ static unsigned long timeStart = 0;
 static const int pump2io[NPUMPS] = {D0, D1, A0, A1, A4, A5, A6, A7};
 
 /* Function prototypes -------------------------------------------------------*/
-int tinkerDigitalRead(String pin);
-int tinkerDigitalWrite(String command);
-int tinkerAnalogRead(String pin);
-int tinkerAnalogWrite(String command);
 int drinkbotSetPumps(String command);
 int drinkbotDebug(String command);
 
@@ -102,13 +100,6 @@ void setup()
 
 	//Setup the Tinker application here
 	RGB.brightness(12);
-
-	//Register all the Tinker functions
-	Spark.function("digitalread", tinkerDigitalRead);
-	Spark.function("digitalwrite", tinkerDigitalWrite);
-
-	Spark.function("analogread", tinkerAnalogRead);
-	Spark.function("analogwrite", tinkerAnalogWrite);
 
 	//Register all the Drinkbot functions
 	Spark.function("setpumps", drinkbotSetPumps);
@@ -171,128 +162,6 @@ void loop()
 }
 
 /*******************************************************************************
- * Function Name  : tinkerDigitalRead
- * Description    : Reads the digital value of a given pin
- * Input          : Pin
- * Output         : None.
- * Return         : Value of the pin (0 or 1) in INT type
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerDigitalRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber < 0 || pinNumber > 7)
-		return -1;
-
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT_PULLDOWN);
-		return digitalRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber + 10, INPUT_PULLDOWN);
-		return digitalRead(pinNumber + 10);
-	}
-	return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerDigitalWrite
- * Description    : Sets the specified pin HIGH or LOW
- * Input          : Pin and value
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerDigitalWrite(String command)
-{
-	bool value = 0;
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber < 0 || pinNumber > 7) return -1;
-
-	if(command.substring(3, 7) == "HIGH") value = 1;
-	else if(command.substring(3, 6) == "LOW") value = 0;
-	else return -2;
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		digitalWrite(pinNumber, value);
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber + 10, OUTPUT);
-		digitalWrite(pinNumber + 10, value);
-		return 1;
-	}
-	else return -3;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogRead
- * Description    : Reads the analog value of a pin
- * Input          : Pin
- * Output         : None.
- * Return         : Returns the analog value in INT type (0 to 4095)
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerAnalogRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber < 0 || pinNumber > 7) return -1;
-
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT);
-		return analogRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber + 10, INPUT);
-		return analogRead(pinNumber + 10);
-	}
-	return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogWrite
- * Description    : Writes an analog value (PWM) to the specified pin
- * Input          : Pin and Value (0 to 255)
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerAnalogWrite(String command)
-{
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber < 0 || pinNumber > 7) return -1;
-
-	String value = command.substring(3);
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		analogWrite(pinNumber, value.toInt());
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber + 10, OUTPUT);
-		analogWrite(pinNumber + 10, value.toInt());
-		return 1;
-	}
-	else return -2;
-}
-
-/*******************************************************************************
  * Function Name  : drinkbotSetPumps
  * Description    : Setup and start the pumping jobs,
  *                  e.g. P0:xx;P1:xx;P2:xx;P3:xx;P4:xx;P5:xx;P6:xx;P7:xx
@@ -310,8 +179,8 @@ int drinkbotSetPumps(String command)
 	Serial.println(command);
 
 	if (command.length() != 47) {
-//		Serial.println("Please use correct format for args, e.g.");
-//		Serial.println("\tP0:10;P1:00;P2:20;P3:05;P4:07;P5:15;P6:00;P7:10");
+		Serial.println("Please use correct format for args, e.g.");
+		Serial.println("\tP0:10;P1:00;P2:20;P3:05;P4:07;P5:15;P6:00;P7:10");
 		return -EINVAL;
 	}
 
